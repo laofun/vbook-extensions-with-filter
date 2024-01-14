fa649831cddd1bc893ac299719a2426ec644d81f
laofun <laofun@gmail.com>
2024-01-14 17:41:35 +0700

    update nettruyenclub

--- nettruyen/src/config.js
+++ nettruyen/src/config.js
@@ -2,7 +2,7 @@ var excludedIds = [2,12,13,15,18,27,34,38,40,42,44,45,46,58,59,60];
 var notgenres = '2,12,13,15,18,27,34,38,40,42,44,45,46,58,59,60'
 // Danh sách các tên thể loại không phù hợp cho trẻ em
 var excludedCategories = {}
-let BASE_URL = 'https://www.nettruyenbing.com';
+let BASE_URL = 'https://www.nettruyenclub.com';
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
