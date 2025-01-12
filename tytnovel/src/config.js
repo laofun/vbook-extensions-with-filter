5152a6aef8fb3a291f8ef229fbf23ffdeb593fcb
laofun <laofun@gmail.com>
2025-01-12 22:58:17 +0700

    test token

--- tytnovel/src/config.js
+++ tytnovel/src/config.js
@@ -1,7 +1,7 @@
 let BASE_URL = "https://tytnovel.xyz";
 let BASE_HOST = "https://api.noveltyt.xyz";
 let BASE_TOKEN = "c8433c0dbef937654e982d076e4f4320b71ab3d2c32d7329ec48b120dacc1f37";
-let BASE_USER = "66d95cf231c59f350f798376";
+let BASE_USER = "6749c0fb47fe46519f18a1f8";
 let BASE_VERSION = "80";
 try {
     if (CONFIG_URL) {
